dddeec7a03a679cf8864ba2e16eeed9c00a98f08
Brandon Bakus <brandonbakus@gmail.com>
2025-01-30 10:05:57 -0500

    03_starter

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
+
 function Pet({pet}){
+
+    
+
+    const [numberOfLikes, setNumberOfLikes] = useState(0)
+
+
+    function increaseLikes(){
+        setNumberOfLikes(numberOfLikes + 1)
+        
+    }
+
+    const [displayName, setDisplayName] = useState(true)
+
+    function toggleDisplay(){
+        setDisplayName((displayName) => !displayName)
+    }
+
     return (
         <li className="pet">
             <img src={pet.image} alt={pet.name}/>
-            <h4 className="">{pet.name}</h4>
-            <button className="like-button">0 Likes</button>
+            <h4 onClick={toggleDisplay} className={displayName ? "" : pet.animal_type}>{displayName ? pet.name : pet.animal_type}</h4>
+            <button onClick={increaseLikes} className="like-button">{numberOfLikes} Likes</button>
         </li>
     );
 }
